cf01d04070b8df8c3848f319baca32c7b66a50f6
HaSanAlkholy <hassan.m.h.m.a123@gmail.com>
2020-02-16 06:48:34 -0800

    ..

---
+++ assets/scripts/themechange.js
@@ -0,0 +1,24 @@
+
+// if (localStorage.getItem("header-style")!==null) {
+//      document.querySelector('.app-header').className=localStorage.getItem("header-style");
+// }
+// if (localStorage.getItem("sidebar-style")!==null) {
+//      document.querySelector('.app-sidebar').className=localStorage.getItem("sidebar-style");
+// }
+
+
+
+// var a =document.querySelectorAll('.theme-settings-swatches');
+
+// a.forEach(addEvent);
+
+// function addEvent (i) {
+// i.addEventListener("click",change);
+// }
+
+// function change (){
+// localStorage.setItem("header-style", '"'+document.querySelector('.app-header').className+'"');
+// localStorage.setItem("sidebar-style",'"'+document.querySelector('.app-sidebar').className+'"');
+// }
+
+
